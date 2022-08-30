508e079972209d0a263ce1a82917e97f8f86926f
Henry Benaye <57096397+HenryBenaye@users.noreply.github.com>
2022-08-30 12:43:01 +0200

    Update webbrowser.py

--- webbrowser.py
+++ webbrowser.py
@@ -5,3 +5,6 @@ import webbrowser
 def web_browser():
     # Define the url where you wanna go
     url = "https://jarvis.bit-academy.nl/login"
+
+    # Search your path to the chrome.exe
+    chrome_path = 'C:/Program Files/Google/Chrome/Application/chrome.exe %s'
